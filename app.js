8ef087c2e850d012f51bb5b292bb794719bb3710
Rodrigo Lima <rodrigodaschagas1234@gmail.com>
2022-12-27 14:41:26 -0300

    Quiz application created

---
+++ app.js
@@ -0,0 +1,54 @@
+const form = document.querySelector(".quiz-form");
+const resultContainer = document.querySelector(".result-container");
+const correctQuestions = ["D", "C", "A", "B"];
+
+let score = 0;
+
+const inseriTxtInSpan = (value, text) =>
+  (value.querySelector("span").textContent = `${text}`);
+
+const resultAnimation = (n, max)=>{
+  const cont = setInterval(() => {
+    if (n === max) {
+      clearInterval(cont);
+    }
+    inseriTxtInSpan(resultContainer, n+"%");
+    n++;
+  }, 10);
+};
+
+const getUserAnswers = ()=>{
+  let userAnswers = [];
+  correctQuestions.forEach((_, index)=>{
+    userAnswers.push(form[`inputQuestion${index + 1}`].value);
+  })
+ 
+  return userAnswers;
+}
+
+const finalResult = (answersQuestions)=>{
+  answersQuestions.forEach((answerQuestion, index) => {
+    const verification = answerQuestion === correctQuestions[index];
+    if (verification) {
+      score += 25;
+    }
+  });
+}
+
+
+const explainResult = ()=> {
+  resultContainer.classList.remove("d-none");
+  scroll({
+    top:0,
+    left:0,
+    behavior:'smooth'
+  });
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const answersQuestion = getUserAnswers();
+  finalResult(answersQuestion);
+  explainResult();
+  console.log(score);
+  resultAnimation(0,score);  
+});
