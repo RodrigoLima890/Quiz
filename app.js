5bfdb5053b14144ecf466bbb574f5e0fc130f989
Rodrigo Lima <rodrigodaschagas1234@gmail.com>
2022-12-28 18:13:23 -0300

    melhoramento no código

--- app.js
+++ app.js
@@ -7,48 +7,40 @@ let score = 0;
 const inseriTxtInSpan = (value, text) =>
   (value.querySelector("span").textContent = `${text}`);
 
-const resultAnimation = (n, max)=>{
+const resultAnimation = (n, max) => {
   const cont = setInterval(() => {
     if (n === max) {
       clearInterval(cont);
     }
-    inseriTxtInSpan(resultContainer, n+"%");
+    inseriTxtInSpan(resultContainer, n + "%");
     n++;
   }, 10);
 };
 
-const getUserAnswers = ()=>{
-  let userAnswers = [];
-  correctQuestions.forEach((_, index)=>{
-    userAnswers.push(form[`inputQuestion${index + 1}`].value);
-  })
- 
-  return userAnswers;
-}
+const getUserAnswers = () =>
+  correctQuestions.map((_, index) => form[`inputQuestion${index + 1}`].value);
 
-const finalResult = (answersQuestions)=>{
+const finalResult = (answersQuestions) => {
   answersQuestions.forEach((answerQuestion, index) => {
     const verification = answerQuestion === correctQuestions[index];
     if (verification) {
       score += 25;
     }
   });
-}
+};
 
-
-const explainResult = ()=> {
+const explainResult = () => {
   resultContainer.classList.remove("d-none");
   scroll({
-    top:0,
-    left:0,
-    behavior:'smooth'
+    top: 0,
+    left: 0,
+    behavior: "smooth",
   });
-}
+};
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const answersQuestion = getUserAnswers();
   finalResult(answersQuestion);
   explainResult();
-  console.log(score);
-  resultAnimation(0,score);  
+  resultAnimation(0, score);
 });
